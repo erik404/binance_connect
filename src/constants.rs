ada6ca9f23fc8be1c266eb4b8759e2219cee8cc6
Erik-Jan van de Wal <ejvandewal@gmail.com>
2023-09-29 21:08:37 -0600

    Moved error message to constant

--- src/constants.rs
+++ src/constants.rs
@@ -6,3 +6,7 @@ pub const BASE_URL_FUTURES_TESTNET: &str = "https://testnet.binancefuture.com";
 pub const WS_URL_FUTURES_TESTNET: &str = "wss://stream.binancefuture.com";
 /** ENDPOINTS **/
 pub const FUTURES_LISTEN_KEY: &str = "/fapi/v1/listenKey";
+
+/* --- ERROR MESSAGES --- */
+pub const ERR_ON_NO_ENABLED_STREAM_UNAUTHENTICATED_REQ: &str =
+    "Can't start unauthenticated WS connection without at least 1 enabled stream";
